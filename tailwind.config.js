be12b38b813db1b0fe43ca5508cff0cbbc15bc87
Arjun_Lubana <arjunlubana562@gmail.com>
2021-03-09 08:48:07 +0300

    Updated Typography of the UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'display': ['Ribeye Marrow', 'serif'],
+      'body': ['Montserrat', 'sans-serif']
+    },
     extend: {},
   },
   variants: {
